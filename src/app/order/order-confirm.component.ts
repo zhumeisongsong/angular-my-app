7ab5a4e2548b5b40219dd0cdfa57a5ce30824a8f
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-10-11 14:04:29 +0800

    10.11order confirm

--- src/app/order/order-confirm.component.ts
+++ src/app/order/order-confirm.component.ts
@@ -1,17 +1,32 @@
-import{Component,OnInit} from '@angular/core';
+import{Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'order-confirm',
   templateUrl: './order-confirm.component.html',
-  styleUrls: ['./order-pd-list.component.scss','./order-confirm.component.scss',]
+  styleUrls: ['./order-pd-list.component.scss', './order-confirm.component.scss',]
 })
 
-export class OrderConfirmComponent implements OnInit{
-  hasDefaultAddress:boolean;
-  //取userId 查询有没有default address
+export class OrderConfirmComponent implements OnInit {
+  hasDefaultAddress: boolean;
 
-  ngOnInit(){
-    this.hasDefaultAddress=false;
+  getUseId() {
+    let user = '1';
+    localStorage.set('userId', user);
+
+    let userId = localStorage.getItem('userId');
+    console.log(userId);
+    return userId;
+  }
+
+  getDefaultAddress() {
+    this.hasDefaultAddress = false;
+  }
+
+
+
+  //取userId 查询有没有default address
+  ngOnInit() {
+
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,21 +1,57 @@
 import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import {Order} from './order';
-import {ORDERS} from './mock-order';
-let ordersPromise = Promise.resolve(ORDERS);
 
 @Injectable()
+
 export class OrderService {
 
-  // getOrders(status:string):Promise<Order>{
-  //   // return this.getOrders()
-  //   //   .tnen(order=>order)
-  // }
-  //
-  //
-  // getOrder(id:number):Promise<Order>{
-  //   return this.getOrders()
-  //     .then(orders=>orders.find(order=>order.odId ===id));
-  // }
+  private headers = new Headers({
+    'Content-Type': 'application/json'
+  });
+  private orderUrl = 'api/orders';
+
+  constructor(private http: Http) {
+  }
+
+  confirm(order:object): Promise<Order> {
+    return this.http
+      .post(this.orderUrl, JSON.stringify(order), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Order)
+      .catch(this.handleError);
+  }
+
+
+  getOrders(state:number):Promise<Order[]>{
+    return this.http.get(this.orderUrl)
+      .toPromise()
+      .then(response=>response.json().data as Order[])
+      .catch(this.handleError);
+  }
+
+  getOrder(id:number):Promise<Order>{
+    const url = `${this.orderUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Order)
+      .catch(this.handleError);
+  }
+
+  delete(id:number): Promise<void> {
+    const url = `${this.orderUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 
 }
